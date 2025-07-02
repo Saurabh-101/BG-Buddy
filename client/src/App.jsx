da7b9f0aae42fff980bc8e1796a3584362af112c
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-07-02 11:51:04 +0530

    Created Navbar and updated the icons and logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='min-h-screen bg-slate-50'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
     </div>
   )
 }
